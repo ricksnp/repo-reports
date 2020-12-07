c05df85aad243a1ac2933fdae44757e5d3d7c4a6
Nathan Ricks <54324815+ricksnp@users.noreply.github.com>
2020-12-07 10:26:08 -0500

    Initial Set-up

---
+++ src/components/input.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const InputElement = () => {
+    return (
+    <Container>
+        <Row>
+            <Col>.col</Col>
+            <Col>.col</Col>
+        </Row>
+
+    </Container>
+    );
+};
+
+export default InputElement;
